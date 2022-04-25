bb452ec95b03890b20643ca58d5cb46e1040a4e5
batpy <batpyxuh2107@icloud.com>
2022-04-25 18:53:48 +0300

    Initial commit DinamicArray

---
+++ src/com/company/Main.java
@@ -0,0 +1,14 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        int[] arr = {1, 2, 3, 4};
+        Arr arr1 = new Arr(arr);
+
+        arr1.printArr();
+        arr1.push(5);
+        arr1.printArr();
+    }
+}
