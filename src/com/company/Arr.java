204e1b907def868f4bf36d290dcd3fdbb6d9a8e8
Klozevitz <Klozevitz@gmail.com>
2022-04-25 19:21:09 +0300

    pushhead added

--- src/com/company/Arr.java
+++ src/com/company/Arr.java
@@ -16,7 +16,6 @@ public class Arr {
         int[] arr1 = new int[length + 1];
         for (int i = 0;i < this.length ;i++){
             arr1[i] = arr[i];
-
         }
         arr1[length] = value;
         this.arr = arr1;
