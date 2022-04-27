68f1e08ed781ab5a7492a25aeb865e3946ce6c44
Klozevitz <Klozevitz@gmail.com>
2022-04-27 16:56:42 +0300

    Не лезь в работу)

--- src/com/company/Arr.java
+++ src/com/company/Arr.java
@@ -3,14 +3,14 @@ package com.company;
 import java.util.Arrays;
 
 public class Arr {
-
     private int[] arr;
     private int length;
-
     public Arr(int[] arr) {
         this.arr = arr;
         this.length = arr.length;
     }
+
+
 
     public void push(int value){
         int[] arr1 = new int[length + 1];
@@ -22,12 +22,10 @@ public class Arr {
         this.length++;
     }
 
-
     public void pushHead(int value) {
         int[] arr1 = new int[length + 1];
         for (int i = 1; i < this.length ; i++){
             arr1[i] = arr[i - 1];
-
         }
         arr1[0] = value;
         this.arr = arr1;
@@ -35,7 +33,6 @@ public class Arr {
     }
 
     public void printArr(){
-
         System.out.println(Arrays.toString(this.arr));
     }
 }
